d03e9c54a03ef6d9add38ff702ac4c5a7aea9485
hoopRenderNyon <Noelle1dr@outlook.com>
2026-08-27 20:55:49 +0800

    Add files via upload

--- SDL_Project.cpp
+++ SDL_Project.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <algorithm>
+#include <cmath>
+#include <vector>
 
 struct _2DLine
 {
     int x, y, x1, y1;
+};
+struct Point{
+    int x, y;
+};
+struct Point3D{
+    int x, y, z;
 };
 
 bool Loop();
 bool Init();
 void Quit();
 
+int POV = 60;
+const Point s_z = {640, 320};
 SDL_Window *window;
 SDL_Renderer *renderer;
+class SDL3D{
+public:
+    Point Trans3D_to_2D(Point3D point){
+        float W = (float)s_z.x;
+        float H = (float)s_z.y;
+        float aspect = W/H;
+
+        float povRad = POV * 3.14159265f / 180.0f;
+        float tanHaif = tan(povRad / 2.0f);
+        if (point.z <= 0.001f) {return {999, 999};}
+
+        float ndc_x = point.x / (point.z * tanHaif * aspect);
+        float ndc_y = point.x / (point.z * tanHaif);
+
+        float x_f = (ndc_x + 1.0f) * (W / 2.0f);
+        float y_f = (1.0f - ndc_y) * (H / 2.0f);
+        
+        int x_int = (int)std::round(x_f);
+        int y_int = (int)std::round(y_f);
+
+        x_int = std::clamp(x_int, 0, ((int)W - 1));
+        y_int = std::clamp(y_int, 0, ((int)W - 1));
+
+        return {x_int, y_int};
+    }
+};
 
 int main(int argc, char** args){
     if (Init()){
@@ -47,7 +84,7 @@ bool Loop(){
 
 bool Init(){
     SDL_Init(SDL_INIT_EVERYTHING);
-    int result = SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+    int result = SDL_CreateWindowAndRenderer(s_z.x, s_z.y, 0, &window, &renderer);
     if (result != 0){
         std::cout << "Feiled of Window and Render: %s" << SDL_GetError();
         return false;
